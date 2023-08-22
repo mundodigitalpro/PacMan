66b20d3c33b4f2d217f9d6581a95be45fbc8deff
Jose Jordan <mundodigital.pro@gmail.com>
2023-08-22 12:04:12 +0200

    update

--- app/src/main/java/com/josejordan/pacman/GameView.kt
+++ app/src/main/java/com/josejordan/pacman/GameView.kt
@@ -5,26 +5,33 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Point
+import android.view.MotionEvent
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 import java.util.Random
+import kotlin.math.abs
 import kotlin.math.min
 
 class GameView(context: Context) : SurfaceView(context), Runnable {
+
     private var running = false
-    private val thread: Thread
+    private val thread: Thread = Thread(this)
     private val paint: Paint = Paint()
-    private val gridSize = Point(28, 31)
+    private val gridSize = Point(30, 31)
     private val random = Random()
+    private var cellSize: Float = 0.0f
+    private var pacManDirection = Point(0, 0)
+    private var offsetX: Float = 0.0f
+    private var offsetY: Float = 0.0f // Aquí se declara la variable offsetY
 
     // Añadir Pac-Man y un fantasma
     private val pacMan = PacMan(Point(14, 15))
     private val ghost = Ghost(Point(14, 10), Color.RED)
     private val maze = Maze() // Añadir instancia del laberinto
 
+
     init {
-        thread = Thread(this)
-        val surfaceHolder: SurfaceHolder = holder.apply {
+        holder.apply {
             addCallback(object : SurfaceHolder.Callback {
                 override fun surfaceCreated(holder: SurfaceHolder) {
                     running = true
@@ -39,6 +46,36 @@ class GameView(context: Context) : SurfaceView(context), Runnable {
         }
     }
 
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        // Verificar si el usuario ha tocado la pantalla
+        if (event?.action == MotionEvent.ACTION_DOWN) {
+            val touchX = event.x
+            val touchY = event.y
+            val pacManCenterX = pacMan.position.x.toFloat() * cellSize + cellSize / 2 + offsetX
+            val pacManCenterY = pacMan.position.y.toFloat() * cellSize + cellSize / 2 + offsetY
+
+            // Calcular la diferencia en x y y entre el toque del usuario y la posición actual de Pacman
+            val deltaX = touchX - pacManCenterX
+            val deltaY = touchY - pacManCenterY
+
+            // Verificar si el usuario ha tocado cerca de Pacman y actualizar la dirección en consecuencia
+            if (abs(deltaX) > abs(deltaY)) {
+                if (deltaX > 0) {
+                    pacManDirection = Point(1, 0)
+                } else {
+                    pacManDirection = Point(-1, 0)
+                }
+            } else {
+                if (deltaY > 0) {
+                    pacManDirection = Point(0, 1)
+                } else {
+                    pacManDirection = Point(0, -1)
+                }
+            }
+        }
+        return true
+    }
+
     override fun run() {
         while (running) {
             update()
@@ -48,9 +85,14 @@ class GameView(context: Context) : SurfaceView(context), Runnable {
     }
 
     private fun update() {
-        val newPacManDirection = randomDirection()
-        if (pacMan.canChangeDirection(newPacManDirection, maze)) {
-            pacMan.direction = newPacManDirection
+        // Verificar si se ha actualizado la dirección de Pacman
+        if (pacManDirection.x != 0 || pacManDirection.y != 0) {
+            val newDirection = Point(pacManDirection.x, pacManDirection.y)
+            if (pacMan.canChangeDirection(newDirection, maze)) {
+                if (newDirection != pacMan.direction) {
+                    pacMan.direction = newDirection
+                }
+            }
         }
         val newGhostDirection = randomDirection()
         if (ghost.canChangeDirection(newGhostDirection, maze)) {
@@ -80,12 +122,12 @@ class GameView(context: Context) : SurfaceView(context), Runnable {
         // Calcular el tamaño de la celda en función del tamaño de la pantalla y la relación de aspecto del laberinto
         val cellSizeWidth = width.toFloat() / gridSize.x
         val cellSizeHeight = height.toFloat() / gridSize.y
-        val cellSize = min(cellSizeWidth, cellSizeHeight)
+        cellSize = min(cellSizeWidth, cellSizeHeight)
 
 
         // Calcular el desplazamiento para centrar el laberinto en la pantalla
-        val offsetX = (width - (cellSize * gridSize.x)) / 2
-        val offsetY = (height - (cellSize * gridSize.y)) / 2
+        offsetX = (width - (cellSize * gridSize.x)) / 2
+        offsetY = (height - (cellSize * gridSize.y)) / 2
 
         // Dibujar el laberinto, Pac-Man y el fantasma
         canvas.save()
@@ -136,13 +178,9 @@ class PacMan(val position: Point, var direction: Point = Point(0, 0)) {
     fun canChangeDirection(newDirection: Point, maze: Maze): Boolean {
         val newX = position.x + newDirection.x
         val newY = position.y + newDirection.y
-
         return newX in 0 until maze.columnCount && newY in 0 until maze.rowCount && maze.data[newY][newX] == 0
     }
-
-
 }
-
 class Ghost(val position: Point, val color: Int, var direction: Point = Point(0, 0)) {
     fun update(maze: Maze) {
         val newX = position.x + direction.x
@@ -170,27 +208,28 @@ class Ghost(val position: Point, val color: Int, var direction: Point = Point(0,
     }
 
 }
-
 class Maze {
     val data = arrayOf(
+        intArrayOf(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
         intArrayOf(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
         intArrayOf(1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
         intArrayOf(1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1),
         intArrayOf(1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1),
         intArrayOf(1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1),
-        intArrayOf(1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 1),
+        intArrayOf(1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 1),
         intArrayOf(1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1),
         intArrayOf(1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1),
         intArrayOf(1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
         intArrayOf(1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1),
         intArrayOf(1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1),
         intArrayOf(1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
-        intArrayOf(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
-        intArrayOf(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
-        intArrayOf(1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+        intArrayOf(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+        intArrayOf(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+        intArrayOf(1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
         intArrayOf(1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1),
         intArrayOf(1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1),
         intArrayOf(1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+        intArrayOf(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
         intArrayOf(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
         intArrayOf(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
     )
